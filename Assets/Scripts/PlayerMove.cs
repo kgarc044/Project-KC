4e3c985a8bf68120e3f902d12772fe536e476e85
kgarc044 <46533611+kgarc044@users.noreply.github.com>
2021-11-07 20:22:09 -0800

    Player/Camera scripts added
    Camera follows player (can adjust offset of camera) and player can move side to side

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private float moveX;
+    public int playerSpeed = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PMove();
+    }
+
+    void PMove()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+ }
