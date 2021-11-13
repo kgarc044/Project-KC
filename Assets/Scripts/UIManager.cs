37f7b81a0dff6331afd36b25b37dafdd47e1a054
Christian Locker <christian.locker.430@my.csun.edu>
2021-11-12 20:35:30 -0800

    Added MainMenu scene, and basic UI manager script.
    Added MainMenu scene, and basic UI manager script. Working buttons and scene transition, just need more for the buttons to do. Going to work on pause menu and in game UI later.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonClicked(Button b)
+    {
+        switch (b.name)
+        {
+            case "StartButton":
+                SceneManager.LoadScene(sceneName: "PlayerCamera");
+                break;
+            case "SettingsButton":
+                break;
+            case "QuitButton":
+                UnityEditor.EditorApplication.isPlaying = false;
+                Application.Quit();
+                break;
+        }
+    }
+}
