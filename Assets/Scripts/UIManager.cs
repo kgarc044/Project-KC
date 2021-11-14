a21ab9eb141b4bf85a4bf9c6c8e6fb3c4de2683b
Christian Locker <christian.locker.430@my.csun.edu>
2021-11-14 01:19:05 -0800

    Basic in game HUD + Pause Menu
    Start on the in game HUD, mostly for function at this time. Also created basic pause menu.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,18 +5,48 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System;
 
+
+
 public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject PauseMenu;
+
+    public Boolean PMenuActive;
+    public static bool gameIsPaused;
+
     void Start()
     {
-        
+        Scene currentScene = SceneManager.GetActiveScene();
+        PauseMenu.gameObject.SetActive(false);
+
+        PMenuActive = false;
+        gameIsPaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PMenuActive == true)
+            {
+                PauseMenu.gameObject.SetActive(false);
+                PMenuActive = false;
+                Time.timeScale = 1;
+                gameIsPaused = false;
+            }
+            else if (PMenuActive == false)
+            {
+                PauseMenu.gameObject.SetActive(true);
+                PMenuActive = true;
+                Time.timeScale = 0;
+                gameIsPaused = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
     }
 
     public void ButtonClicked(Button b)
@@ -25,6 +55,12 @@ public class UIManager : MonoBehaviour
         {
             case "StartButton":
                 SceneManager.LoadScene(sceneName: "PlayerCamera");
+                break;
+            case "ContinueButton":
+                PauseMenu.gameObject.SetActive(false);
+                PMenuActive = false;
+                Time.timeScale = 1;
+                gameIsPaused = false;
                 break;
             case "SettingsButton":
                 break;
