fa3bc7b44c65d9059662c89db9e5e243fd0ffd00
kgarc044 <46533611+kgarc044@users.noreply.github.com>
2021-11-16 16:58:52 -0800

    Added tile maps and more player movement stuff

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     private float moveX;
+    public float checkRadius = 0.2f;
+
     public int playerSpeed = 15;
     public int playerJumpPower = 1250;
+
+    public bool FacingRight = true;
+    public bool isGrounded = true;
+
+    [SerializeField]
+    private Transform groundCheck;
+
+    public LayerMask whatisGround;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +26,12 @@ public class PlayerMove : MonoBehaviour
     private void FixedUpdate()
     {
         PMove();
+        CheckForGround();
     }
     // Update is called once per frame
     void Update()
     {
-            if (Input.GetButtonDown("Jump"))
+            if (Input.GetButtonDown("Jump") && isGrounded)
             {
                 Jump();
             }
@@ -29,6 +40,18 @@ public class PlayerMove : MonoBehaviour
     void PMove()
     {
         moveX = Input.GetAxis("Horizontal");
+
+        if (moveX < 0.0f && FacingRight)
+        {
+            FacingRight = !FacingRight;
+            transform.Rotate(0f, 180f, 0f);
+        }
+
+        if (moveX > 0.0f && !FacingRight)
+        {
+            FacingRight = !FacingRight;
+            transform.Rotate(0f, 180f, 0f);
+        }
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
     }
 
@@ -36,5 +59,10 @@ public class PlayerMove : MonoBehaviour
     {
         //Jumping Code
         GetComponent<Rigidbody2D>().AddForce(new Vector2(0, playerJumpPower));
+    }
+
+    void CheckForGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatisGround);
     }
 }
